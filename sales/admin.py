132a24ecb7594c0dff7689f29fb06f6444905f59
bnutss <b.nutss@mail.com>
2024-10-14 09:15:18 +0500

    new commit

--- sales/admin.py
+++ sales/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import Order, OrderProduct
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'quantity', 'price', 'created_at']
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'vat', 'is_confirmed', 'confirmed_at', 'is_rejected', 'rejected_at', 'created_at']
+    list_filter = ['client', 'is_confirmed', 'is_rejected']
+    search_fields = ['client', ]
+
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['order', 'name', 'quantity', 'price', 'created_at']
+    list_filter = ['order', ]
+    search_fields = ['order', 'name']
+    autocomplete_fields = ['order']
--- sales/models.py
+++ sales/models.py
@@ -2,7 +2,37 @@ from django.db import models
 from django.utils import timezone
 
 
-class Product(models.Model):
+class Order(models.Model):
+    client = models.CharField(max_length=100, verbose_name='Название клиента')
+    vat = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='НДС (%)')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_confirmed = models.BooleanField(default=False, verbose_name="Подтвержденный заказ")
+    is_rejected = models.BooleanField(default=False, verbose_name="Отклоненный заказ")
+    confirmed_at = models.DateTimeField(null=True, blank=True, verbose_name="Дата подтверждения")
+    rejected_at = models.DateTimeField(null=True, blank=True, verbose_name="Дата отклонения")
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f"Заказ {self.id} от {self.client}"
+
+    def save(self, *args, **kwargs):
+        if self.is_confirmed and self.confirmed_at is None:
+            self.confirmed_at = timezone.now()
+
+        if self.is_rejected and self.rejected_at is None:
+            self.rejected_at = timezone.now()
+
+        if self.is_confirmed and self.is_rejected:
+            raise ValueError("Заказ не может быть одновременно подтвержденным и отклоненным.")
+
+        super(Order, self).save(*args, **kwargs)
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='products', verbose_name="Заказ")
     name = models.CharField(max_length=255, verbose_name="Название")
     quantity = models.PositiveIntegerField(verbose_name="Количество")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
@@ -14,4 +44,4 @@ class Product(models.Model):
         verbose_name_plural = 'Продукты'
 
     def __str__(self):
-        return self.name
+        return f'{self.name} - {self.order}'
