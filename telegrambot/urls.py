39fc8733daf5e3b9fb42b8c30c893f8e4faba7d7
bnutss <b.nutss@mail.com>
2025-09-23 13:26:16 +0500

    fixed

--- telegrambot/urls.py
+++ telegrambot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import simple_views
 
+app_name = 'telegrambot'
 urlpatterns = [
     path('webhook/', simple_views.telegram_webhook, name='telegram_webhook'),
 ]
